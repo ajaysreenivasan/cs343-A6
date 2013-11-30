f06f0860c08f0f672e60c1cd269f253a1787a0ec
sdermott <scott_k_dermott@hotmail.com>
2013-11-29 21:52:43 -0500

    started implementation of student

--- student.cc
+++ student.cc
@@ -1,12 +1,32 @@
 #include "student.h"
+#include "MPRNG.h"
+#include "vendingMachine.h"
 
 using namespace std;
 
+extern MPRNG m;
+
 void Student::main(){
+	
+	int favFlav = m(3);					 	//random favorite flavour between [0,3]
+	//this should be a flavour 
+
+	wco->create(id,5);					//create new watCard for student with 5$
+	//possible that watcard is lost by courier -> need to account for this
+
+	VendingMachine * vmLocation=ns->getMachine(id);			//get vending machine location via name server
+
+	while(numToPurchase>0){
+
+	}
 
 }
 
-Student::Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id,
+Student::Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int _id,
              unsigned int maxPurchases ){
-
+	p=&prt;
+	ns=&nameServer;
+	wco=&cardOffice;
+	id=_id;
+	numToPurchase=m(1,maxPurchases); 	//random number bottles to purchase between 1 and maxPurchases
 }
--- student.h
+++ student.h
@@ -4,9 +4,15 @@
 #include "printer.h"
 #include "nameServer.h"
 #include "watCardOffice.h"
+#include "watcard.h"
 
 _Task Student {
     void main();
+    Printer * p;
+    NameServer * ns;
+    WATCardOffice * wco;
+    WATCard::FWATCard fw;
+    unsigned int id, numToPurchase;
   public:
     Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id,
              unsigned int maxPurchases );
