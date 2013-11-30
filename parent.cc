4ace4deb7e3fd881aca8ebc5d2c326f7d96285cf
ajaysreenivasan <ajaysreenivasan@gmail.com>
2013-11-30 02:55:44 -0500

    Bank done. Parent done. Also, I'm getting hungry...

--- parent.cc
+++ parent.cc
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// constructo
+// constructor
 Parent::Parent(Printer& prt, Bank &bank, unsigned int numStudents, unsigned int parentalDelay): 
 	printer(prt),
 	bank(bank)
@@ -12,5 +12,15 @@ Parent::Parent(Printer& prt, Bank &bank, unsigned int numStudents, unsigned int 
 }
 
 void Parent::main(){
-
+	while(true){
+		_Accept(~Parent){
+			break;
+		}
+		_Else{
+			unsigned int depositRecipient = rng(0, numStudents);
+			unsigned int depositAmount = rng(1, 3);
+			yield(parentalDelay);
+			bank.deposit(depositRecipient, depositAmount);
+		}
+	}
 }
