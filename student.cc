1de806d044875fa04120154b18bfc255201d1d8c
sdermott <scott_k_dermott@hotmail.com>
2013-12-01 21:34:28 -0500

    On 6th litre of chocolate milk implemented student

--- student.cc
+++ student.cc
@@ -1,51 +1,71 @@
 #include "student.h"
 #include "vendingMachine.h"
+#include "watCardOffice.h"
 
 Student::Student(Printer& prt, NameServer& nameServer, WATCardOffice& cardOffice, unsigned int id,
 				 unsigned int maxPurchases):
-printer(prt),
-	nameServer(nameServer), 
-	cardOffice(cardOffice)
+									printer(prt),
+									nameServer(nameServer), 
+									cardOffice(cardOffice)
 {
-	id = id;
-	numToPurchase = rng(1, maxPurchases); 	//random number bottles to purchase between 1 and maxPurchases
+	this->id = id;
 }
 
 void Student::main(){
-	int favFlav = rng(3);					 	//random favorite flavour between [0,3]
-	//this should be a flavour 
-	fWATCard = cardOffice.create(id, 5);					//create new watCard for student with 5$
-	//possible that watcard is lost by courier . need to account for this
+	numToPurchase = rng(1, maxPurchases); 	//random number bottles to purchase between 1 and maxPurchases			 	
+	VendingMachine::Flavours favouriteFlavour = (VendingMachine::Flavours)rng(3); //random favorite flavour between [0,3]
+	//this should be a flavour
+	bool watCardLost=true;					//set to true to do initial create
+	
+	while(watCardLost){												//possible that watcard is lost by courier . need to account for this
+		try{
+			fWATCard = cardOffice.create(id, 5);					//create new watCard for student with 5$
+		}
+		catch(WATCardOffice::Lost){
+			watCardLost=true;									//new watcard was lost retry creating with 5$
+		}
+	}
+	
 
 	VendingMachine* vmLocation = nameServer.getMachine(id);			//get vending machine location via name server
-
+	bool noYield=false;			//noYield used to tell not needing to yield
 	while(numToPurchase > 0){
 		//attempt to purchase soda
 		//yield random number of times between 1 and 10
-		yield(rng(1, 10));
-
+		if(!noYield){
+			yield(rng(1, 10));
+		}
+		noYield=false;																//set to false for now to indicate next loop through it will need to yield
 		//attempt to buy the soda from machine
-		//VendingMachine::Status stat= vmLocation->buy(favFlav, fWATCard); 
+		VendingMachine::Status stat= vmLocation->buy(favouriteFlavour, *fWATCard()); 
 		//not compiling
 
-		/*if(stat==VendingMachine::STOCK){
-		//get new vending machine if stat == STOCK
-		vmLocation=nameServer.getMachine(id);
+		if(stat==VendingMachine::STOCK){
+			//get new vending machine if stat == STOCK
+			vmLocation=nameServer.getMachine(id);
 		}
 		else if(stat==VendingMachine::FUNDS){
 		//block while update to funds is made need to account for different cases here
-		/*try{
-		fWATCard=cardOffice.tranameServerfer(id,vmLocation.cost()+5);
+			try{
+				fWATCard=cardOffice.transfer(id,vmLocation->cost()+5,fWATCard());
+			}
+			catch(WATCardOffice::Lost){
+				watCardLost=true;												//watcard lost, create new watcard
+				while(watCardLost){
+					try{
+						fWATCard = cardOffice.create(id, 5);					//create new watCard for student with 5$
+					}
+					catch(WATCardOffice::Lost){
+						watCardLost=true;									//new watcard was lost retry creating with 5$
+					}
+				}
+			}
+			noYield=true;													//no yeild to allow student to recall buy without having to yield
 		}
-		catch(Exception e){ 
-
-		}*/
-		/*}
-		else{
-		//stat == BUY successfully purchased soda
-		numToPurchase--;
-		}*/
-	}
+		else{	//stat == BUY successfully purchased soda
+			numToPurchase--;
+		}
+	}//end purchase while loop
 
 	//all soda planned to purchase is purchased terminate
 }
